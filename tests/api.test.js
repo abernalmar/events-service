cd703dc48e35dc38e997748019f400caa42d30ff
abernalmar <abernalmar@gmail.com>
2023-12-18 19:43:43 +0100

    Tests in progress

--- tests/api.test.js
+++ tests/api.test.js
@@ -12,5 +12,41 @@ describe("Events API", () => {
         });
     });
 
+    describe("GET /events", () => {
+        it("should return a list of events", async () => {
+            const response = await request(app).get("/events");
+    
+            expect(response.status).toBe(200);
+            expect(response.body).toBeInstanceOf(Array);
+        }, 10000);
+    });
+    
+    describe("POST /events", () => {
+        it("should create a new event", async () => {
+            const eventData = {
+                name: "Test Event",
+                place: "Test Place"
+            };
+
+            const response = await request(app)
+                .post("/events")
+                .send(eventData);
+
+            expect(response.status).toBe(201);
+        });
+
+        it("should return 400 for incomplete event data", async () => {
+            const incompleteEventData = {
+                name: "Test Event"
+                // Missing 'place' field intentionally
+            };
+
+            const response = await request(app)
+                .post("/events")
+                .send(incompleteEventData);
+
+            expect(response.status).toBe(400);
+        });
+    });
 
 });

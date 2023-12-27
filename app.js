aa7e312e1f6a0cfc4e2e3609bac1ca12ce62f12f
abernalmar <abernalmar@gmail.com>
2023-12-27 20:37:40 +0100

    Modificado app.js

--- app.js
+++ app.js
@@ -1,31 +1,33 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var eventsRouter = require('./routes/events');
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
+var eventsRouter = require("./routes/events");
 
 var app = express();
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/events', eventsRouter);
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+app.use("/api/v1/events", eventsRouter);
 
 //Connection to mongo
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const DB_URL = "mongodb+srv://eventos:eventos23@events-service.spgszx5.mongodb.net/?retryWrites=true&w=majority"
+const DB_URL =
+  "mongodb+srv://eventos:eventos23@events-service.spgszx5.mongodb.net/?retryWrites=true&w=majority";
+console.log("Connecting: %s", DB_URL);
 mongoose.connect(DB_URL);
 const db = mongoose.connection;
 
-db.on('error', console.error.bind(console, 'db connection error'));
+db.on("error", console.error.bind(console, "db connection error"));
 
 module.exports = app;

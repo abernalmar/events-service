e1b2348bf28daff24f9fc3dd2ed7c0729a244c2b
abernalmar <abernalmar@gmail.com>
2023-12-28 14:04:54 +0100

    Conexión con frontend

--- app.js
+++ app.js
@@ -2,9 +2,9 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
+var cors = require("cors");
 
 var indexRouter = require("./routes/index");
-var usersRouter = require("./routes/users");
 var eventsRouter = require("./routes/events");
 
 var app = express();
@@ -14,9 +14,13 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
+app.use(
+  cors({
+    origin: "http://localhost:3001",
+  })
+);
 
 app.use("/", indexRouter);
-app.use("/users", usersRouter);
 app.use("/api/v1/events", eventsRouter);
 
 //Connection to mongo

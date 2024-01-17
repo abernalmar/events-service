cc8fac8e0dcab351002a04bcac82e803d49ea6c1
abernalmar <abernalmar@gmail.com>
2024-01-17 17:03:52 +0100

    Más tests añadidos

--- tests/api.test.js
+++ tests/api.test.js
@@ -20,21 +20,32 @@ describe("Events API", () => {
       expect(response.status).toBe(200);
       expect(response.body).toBeInstanceOf(Array);
     });
+
+    it("should return 404 for a non-existing event", async () => {
+      const nonExistingEventName = "NonExistingEvent";
+      const response = await request(app).get(
+        `/api/v1/events/${nonExistingEventName}`
+      );
+
+      expect(response.status).toBe(404);
+      expect(response.body.error).toBe("Event not found");
+    });
   });
 
   describe("POST /events", () => {
-    /* DA ERROR LA DATE, NO SÉ EN QUÉ FORMATO PONERLA
     it("should create a new event", async () => {
       const eventData = {
         name: "Test Event",
         place: "Test Place",
-        date: new Date("2023-01-01T12:00:00Z"),
+        date: new Date(2024, 3, 31),
       };
 
-      const response = await request(app).post("/events").send(eventData);
+      const response = await request(app)
+        .post("/api/v1/events")
+        .send(eventData);
 
       expect(response.status).toBe(201);
-    });*/
+    });
 
     it("should return 400 for incomplete event data", async () => {
       const incompleteEventData = {
@@ -77,5 +88,114 @@ describe("Events API", () => {
 
       expect(response.status).toBe(400);
     });*/
+  });
+
+  describe("PUT /api/v1/events/:name", () => {
+    it("should update an existing event", async () => {
+      const eventNameToUpdate = "EventToUpdate";
+
+      const assistantObjectId1 = await Event.findOne({
+        name: "UpdatedAssistant1",
+      }).select("_id");
+      const assistantObjectId2 = await Event.findOne({
+        name: "UpdatedAssistant2",
+      }).select("_id");
+
+      // Crea un evento para ser actualizado
+      const eventToUpdate = new Event({
+        name: eventNameToUpdate,
+        place: "Test Place",
+        date: new Date(2021, 3, 31),
+        description: "Test Description",
+        category: "Test Category",
+        assistants: [assistantObjectId1, assistantObjectId2],
+      });
+      await eventToUpdate.save();
+
+      const updatedEventData = {
+        place: "Updated Place",
+        description: "Updated Description",
+        category: "Updated Category",
+        assistants: [
+          await Event.findOne({ name: "UpdatedAssistant1" }).select("_id"),
+          await Event.findOne({ name: "UpdatedAssistant2" }).select("_id"),
+        ],
+      };
+
+      const response = await request(app)
+        .put(`/api/v1/events/${eventNameToUpdate}`)
+        .send(updatedEventData);
+
+      expect(response.status).toBe(200);
+      expect(response.body.name).toBe(eventNameToUpdate);
+      expect(response.body.place).toBe(updatedEventData.place);
+      expect(response.body.description).toBe(updatedEventData.description);
+      expect(response.body.category).toBe(updatedEventData.category);
+      expect(response.body.assistants).toEqual(updatedEventData.assistants);
+    });
+
+    it("should return 404 for updating a non-existing event", async () => {
+      const nonExistingEventName = "NonExistingEvent";
+      const updatedEventData = {
+        place: "Updated Place",
+        description: "Updated Description",
+        category: "Updated Category",
+        assistants: ["UpdatedAssistant1", "UpdatedAssistant2"],
+      };
+
+      const response = await request(app)
+        .put(`/api/v1/events/${nonExistingEventName}`)
+        .send(updatedEventData);
+
+      expect(response.status).toBe(404);
+      expect(response.body.error).toBe("Event not found");
+    });
+  });
+
+  describe("DELETE /api/v1/events/:name", () => {
+    it("should delete an existing event", async () => {
+      const eventNameToDelete = "EventToDelete";
+
+      // Busca o crea ObjectId válidos
+      const assistantObjectId1 = await Event.findOne({
+        name: "Assistant1",
+      }).select("_id");
+      const assistantObjectId2 = await Event.findOne({
+        name: "Assistant2",
+      }).select("_id");
+
+      // Crea un evento para ser eliminado
+      const eventToDelete = new Event({
+        name: eventNameToDelete,
+        place: "Test Place",
+        date: new Date(2024, 3, 19),
+        description: "Test Description",
+        category: "Test Category",
+        assistants: [assistantObjectId1, assistantObjectId2],
+      });
+
+      await eventToDelete.save();
+
+      const response = await request(app).delete(
+        `/api/v1/events/${eventNameToDelete}`
+      );
+
+      expect(response.status).toBe(200);
+      expect(response.body.name).toBe(eventNameToDelete);
+
+      // Verifica que el evento realmente se eliminó de la base de datos
+      const deletedEvent = await Event.findOne({ name: eventNameToDelete });
+      expect(deletedEvent).toBeNull();
+    });
+
+    it("should return 404 for a non-existing event", async () => {
+      const nonExistingEventName = "NonExistingEvent";
+      const response = await request(app).delete(
+        `/api/v1/events/${nonExistingEventName}`
+      );
+
+      expect(response.status).toBe(404);
+      expect(response.body.error).toBe("Event not found");
+    });
   });
 });

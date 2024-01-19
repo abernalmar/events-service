8664db8c0a2ee30d47c4f7283ce60e15d8f0a54a
fracaralb <fracaralb@us.es>
2024-01-19 13:58:39 +0100

    integration test OK

---
+++ db.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+const DB_URL = (process.env.DB_URL ||"mongodb+srv://eventos:eventos23@events-service.spgszx5.mongodb.net/?retryWrites=true&w=majority");
+
+mongoose.connect(DB_URL);
+const db = mongoose.connection;
+
+// recover from errors
+db.on('error', console.error.bind(console, 'db connection error'));
+
+module.exports = db;
